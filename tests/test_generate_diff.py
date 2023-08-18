dbf106c5ebb913d5240be2d206c22d1297a2c6e2
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-08-19 01:08:09 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,6 +2,6 @@ def test_generate_diff():
     file1 = 'tests/fixtures/file1.json'
     file2 = 'tests/fixtures/file2.json'
     with open('tests/fixtures/result.txt', 'r') as result:
-        result_string = n.join(result.read().splitlines())
+        result_string = "\n".join(result.read().splitlines())
         assert generate_diff(file1, file2) == result_string
 
