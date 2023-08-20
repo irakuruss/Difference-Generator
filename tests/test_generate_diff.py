cfea1f2e1f63db05cea1706411a30b9dd3ff7a36
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-08-20 18:26:00 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,7 +4,9 @@ from gendiff.generate_diff import generate_diff
 def test_generate_diff():
     file1 = 'tests/fixtures/file1.json'
     file2 = 'tests/fixtures/file2.json'
+    file3 = 'tests/fixtures/file1.yml'
+    file4 = 'tests/fixtures/file2.yml'
     with open('tests/fixtures/result.txt', 'r') as result:
         result_string = "\n".join(result.read().splitlines())
         assert generate_diff(file1, file2) == result_string
-
+        assert generate_diff(file3, file4) == result_string
