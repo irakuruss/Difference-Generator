127b6689256b5becdf75840fd0f3459ef1ed1f4a
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-08-22 21:20:36 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,12 +1,19 @@
 from gendiff.generate_diff import generate_diff
 
 
+TEST_FILES = [
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'result.txt'),
+    ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'result.txt'),
+    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'result.txt'),
+    ('tests/fixtures/file3.json', 'tests/fixtures/file4.json', 'result2.txt'),
+    ('tests/fixtures/file3.yml', 'tests/fixtures/file4.yml', 'result2.txt'),
+    ('tests/fixtures/file3.yaml', 'tests/fixtures/file4.yaml', 'result2.txt'),
+]
+
+
 def test_generate_diff():
-    file1 = 'tests/fixtures/file1.json'
-    file2 = 'tests/fixtures/file2.json'
-    file3 = 'tests/fixtures/file1.yml'
-    file4 = 'tests/fixtures/file2.yml'
-    with open('tests/fixtures/result.txt', 'r') as result:
-        result_string = "\n".join(result.read().splitlines())
-        assert generate_diff(file1, file2) == result_string
-        assert generate_diff(file3, file4) == result_string
+    for kit in TEST_FILES:
+        file1, file2, result = kit[0], kit[1], kit[2]
+        with open(result, 'r') as result_file:
+            result_string = "\n".join(result_file.read().splitlines())
+            assert generate_diff(file1, file2) == result_string
