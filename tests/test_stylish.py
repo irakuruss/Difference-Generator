4b3fef1547f52eaead12573720a909ce1dc4f2fd
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-08-23 19:59:05 +0300

    Update test_stylish.py

--- tests/test_stylish.py
+++ tests/test_stylish.py
@@ -2,7 +2,7 @@ from gendiff.formatter.stylish import stringify
 
 
 def test_style_formatter():
-    assert style_formatter(True) == 'true'
-    assert style_formatter(False) == 'false'
-    assert style_formatter(24) == '24'
+    assert stringify(True) == 'true'
+    assert stringify(False) == 'false'
+    assert stringify(24) == '24'
 
