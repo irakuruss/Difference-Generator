f7c5643a104c5076f4e311163f61419acfe45f32
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-08-23 19:28:48 +0300

    Update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,9 +3,9 @@ from gendiff.get_diff_tree import get_diff_tree
 from gendiff.load_data import load_data
 
 
-def generate_diff(path1, path2):
+def generate_diff(path1, path2, formatter='stylish'):
     data1 = load_data(path1)
     data2 = load_data(path2)
     diff_tree = get_diff_tree(data1, data2)
-    result = to_format(diff_tree)
+    result = to_format(diff_tree, formatter)
     return result
