da769653fd79debbd3d136b6af6a0392c99c159b
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-08-29 23:06:34 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -29,6 +29,11 @@ TEST_FILES_EXCEPTION = [
     ('file3.yml', 'file4.yml', 'doc')
 ]
 
+TEST_FILES_EXCEPTION2 = [
+    ('file1.pdf', 'file2.yaml'),
+    ('file3.json', 'file4.pdf')
+]
+
 
 def get_path(filename):
     return os.path.join('tests/fixtures', filename)
@@ -52,3 +57,12 @@ def test_exception(file1, file2, formatter):
             get_path(file2),
             formatter)
         assert str(e.value) == 'Unsupported format'
+
+
+@pytest.mark.parametrize('file1, file2', TEST_FILES_EXCEPTION2)
+def test_exception2(file1, file2):
+    with pytest.raises(ValueError) as e:
+        generate_diff(
+            get_path(file1),
+            get_path(file2))
+        assert str(e.value) == 'Unsupported file format: pdf'
