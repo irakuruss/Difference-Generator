9b5552b42e5483f29d7a059bf78ac81f29761000
Irina Kurus <evans.mira@yandex.ru>
2023-09-20 14:12:16 +0300

    Separation of parsing and reading data

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,11 +1,13 @@
 from gendiff.to_format import to_format
 from gendiff.get_diff_tree import get_diff_tree
 from gendiff.load_data import load_data
+from gendiff.parser import parse, get_ext
 
 
-def generate_diff(path1, path2, formatter='stylish'):
-    data1 = load_data(path1)
-    data2 = load_data(path2)
-    diff_tree = get_diff_tree(data1, data2)
+def generate_diff(file1, file2, formatter='stylish'):
+    file_data1, file_data2 = load_data(file1), load_data(file2)
+    dict1 = parse(file_data1, get_ext(file1))
+    dict2 = parse(file_data2, get_ext(file2))
+    diff_tree = get_diff_tree(dict1, dict2)
     result = to_format(diff_tree, formatter)
     return result
---
+++ gendiff/parser.py
@@ -0,0 +1,17 @@
+import os
+import json
+import yaml
+
+
+def get_ext(path):
+    _, ext = os.path.splitext(path)
+    return ext.strip('.')
+
+
+def parse(data, ext):
+    if ext == 'json':
+        return json.loads(data)
+    elif ext in ('yml', 'yaml'):
+        return yaml.load(data, Loader=yaml.Loader)
+    else:
+        raise ValueError(f'Unsupported file format: {ext}')
